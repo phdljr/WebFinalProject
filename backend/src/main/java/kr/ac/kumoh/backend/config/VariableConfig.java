ad9ff8b9ed29da0f58c003e90aabf4c99c86a277
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-06-05 01:34:09 +0900

    Restore Movies'Poster

--- backend/src/main/java/kr/ac/kumoh/backend/config/VariableConfig.java
+++ backend/src/main/java/kr/ac/kumoh/backend/config/VariableConfig.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 public class VariableConfig {
 
-    public static final String imagePath = ".//backend/src//main//resources//static//movies";
+    public static final String imagePath = ".//backend/src//main//resources//movies";
     public static final int[] ranks = {5, 10, 4, 7, 6, 2, 3, 8, 1, 9};
 }
