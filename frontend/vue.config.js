c236cfffd183381b111024057ae97080bd6c87dd
phdljr <phdljr@naver.com>
2022-06-04 14:16:01 +0900

    Edit outputDir at vue.config.js.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: "..//backend//src//main//resources//static",
   devServer: {
     port: 8081,
   },
