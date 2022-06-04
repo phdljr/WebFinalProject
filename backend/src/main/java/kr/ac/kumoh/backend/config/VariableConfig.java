fd955b3b8bd6ba97a5a5909c7b58056134e7e9c8
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-06-04 19:56:00 +0900

    Make VariableConfig

---
+++ backend/src/main/java/kr/ac/kumoh/backend/config/VariableConfig.java
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.backend.config;
+
+import lombok.Getter;
+
+
+@Getter
+public class VariableConfig {
+
+    private static final String imagePath = ".//backend/src//main//resources//static//movies";
+    private static final int[] ranks = {5, 10, 4, 7, 8, 2, 3, 6, 1, 9};
+}
