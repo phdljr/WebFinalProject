33cff3d211a6f2e27156630a86d25cd98d6fdd31
phdljr <phdljr@naver.com>
2022-06-05 23:42:59 +0900

    Add RegisterView.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,10 +8,12 @@ import BootstrapIcon from "@dvuckovic/vue3-bootstrap-icons";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
+const app = createApp(App);
+app.component("b-icon", BootstrapIcon);
+app.use(store);
+app.use(router);
+app.use(BootstrapVue3);
+app.mount("#app");
 
-createApp(App)
-  .component("b-icon", BootstrapIcon)
-  .use(store)
-  .use(router)
-  .use(BootstrapVue3)
-  .mount("#app");
+app.config.globalProperties.HOST = "http://117.20.209.64:8080"; // 종렬 테스트
+// app.config.globalProperties.HOST = "http://202.31.200.215:8080"; // 민수 테스트
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>로그인 화면</h1>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  beforeCteate() {},
+  cteated() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
