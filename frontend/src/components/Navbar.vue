0258df9103686179fe4f5071a7de8f5983d2d3be
phdljr <phdljr@naver.com>
2022-06-06 22:55:54 +0900

    Add my page.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -17,20 +17,22 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item">
-              <a class="nav-link" @click="$router.push('movie')">영화</a>
+              <a class="nav-link" @click="$router.push('/movie')">영화</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" @click="$router.push('theater')">극장</a>
+              <a class="nav-link" @click="$router.push('/theater')">극장</a>
             </li>
           </ul>
         </div>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" @click="$router.push('register')">회원가입</a>
+              <a class="nav-link" @click="$router.push('/register')"
+                >회원가입</a
+              >
             </li>
             <li class="nav-item">
-              <a class="nav-link" @click="$router.push('login')">로그인</a>
+              <a class="nav-link" @click="$router.push('/login')">로그인</a>
             </li>
           </ul>
         </div>
---
+++ frontend/src/views/MyPageView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mypage">
+    <h3>마이페이지</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  beforeCteate() {},
+  cteated() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+<style></style>
