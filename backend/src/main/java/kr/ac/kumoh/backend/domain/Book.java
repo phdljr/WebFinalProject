9367a4c41ffedeba579b950d3dff500c17a95900
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-06-06 17:43:51 +0900

    User Reserve Book not Contained Movie Info
    각 상화에 맞는 예외 처리도 필요

--- backend/src/main/java/kr/ac/kumoh/backend/domain/Book.java
+++ backend/src/main/java/kr/ac/kumoh/backend/domain/Book.java
@@ -22,13 +22,16 @@ public class Book {
     private int numOfPeople;
     private int price;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<BookDetails> bookDetails = new ArrayList<>();
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<Seat> seats = new ArrayList<>();
+
     public Book(int numOfPeople, int price) {
         this.numOfPeople = numOfPeople;
         this.price = price;
     }
-
-    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
-    private List<Seat> seats = new ArrayList<>();
 
     public void addSeat(Seat seat) {
         seats.add(seat);
--- backend/src/main/java/kr/ac/kumoh/backend/domain/BookDetails.java
+++ backend/src/main/java/kr/ac/kumoh/backend/domain/BookDetails.java
@@ -1,10 +1,16 @@
 package kr.ac.kumoh.backend.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 
 @Entity
 @Table(name = "BOOKING_DETAILS")
+@Setter @Getter
+@NoArgsConstructor
 public class BookDetails {
 
     @Id
