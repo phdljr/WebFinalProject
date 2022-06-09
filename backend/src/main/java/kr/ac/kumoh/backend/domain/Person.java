f8c0beb757d7db90f047db6479e389377cd3fcac
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-06-09 21:42:24 +0900

    Person Entity

---
+++ backend/src/main/java/kr/ac/kumoh/backend/domain/Person.java
@@ -0,0 +1,24 @@
+package kr.ac.kumoh.backend.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+public class Person {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "PERSON_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MOVIE_ID")
+    private Movie movie;
+
+    private String name;
+
+    private String occupation;
+}
