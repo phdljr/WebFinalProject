2c9541216a1b77d617e73c46a5c8b297d0bac536
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-06-09 22:10:32 +0900

    add Genre attr to Movie Class

--- backend/src/main/java/kr/ac/kumoh/backend/TestQuery.sql
+++ backend/src/main/java/kr/ac/kumoh/backend/TestQuery.sql
@@ -10,12 +10,18 @@ SELECT * FROM PERSON;
 SELECT * FROM COMMENTS;
 
 -- 영화 데이터 넣기
-insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE ) values(100, '15세', '2021년 6월 6일', '150분',  '태극기 휘날리며');
-insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE ) values(101, '12세', '2022년 6월 1일', '147분',  '쥬라기 월드-도미니언');
-insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE ) values(102, '15세', '2022년 5월 18일', '106분',  '범죄도시 2');
-insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE ) values(103, '12세', '2022년 6월 8일', '129분',  '브로커');
-insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE ) values(104, '12세', '2021년 6월 1일', '101분',  '카시오페아');
-insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE ) values(105, '15세', '2022년 5월 4일', '126분',  '닥터 스트레인지-대혼돈의 멀티버스');
+insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE, GENRE)
+values(100, '15세', '2021년 6월 6일', '150분',  '태극기 휘날리며', '역사');
+insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE, GENRE)
+values(101, '12세', '2022년 6월 1일', '147분',  '쥬라기 월드-도미니언', '액션, 어드벤처');
+insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE, GENRE)
+values(102, '15세', '2022년 5월 18일', '106분',  '범죄도시 2', '범죄, 액션');
+insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE, GENRE)
+values(103, '12세', '2022년 6월 8일', '129분',  '브로커', '드라마');
+insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE, GENRE)
+values(104, '12세', '2021년 6월 22일', '130분',  '탑건-매버릭', '액션');
+insert into movie(MOVIE_ID, MEDIA_RATING, RELEASE_DATE, RUNTIME, TITLE, GENRE)
+values(105, '15세', '2022년 5월 4일', '126분',  '닥터 스트레인지-대혼돈의 멀티버스', '액션, 어드벤처, 환타지');
 
 -- 영화관 데이터 넣기
 insert into theater(THEATER_ID, FLOOR, SCREEN, NUM_OF_COLUMNS, NUM_OF_ROWS, NUM_OF_SEATS) values(98, 1, '1관', 13, 13, 169);
