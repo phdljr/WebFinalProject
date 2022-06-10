7b93020cba98de602b895b5fa00750f4ecd9d4d8
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-06-11 06:00:05 +0900

    revise findUserComment

--- backend/src/main/java/kr/ac/kumoh/backend/dto/AddLikeDTO.java
+++ backend/src/main/java/kr/ac/kumoh/backend/dto/AddLikeDTO.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class AddLikeDTO {
 
-    private String comment;
-    private String commentDate;
+    private String commentUserId;
+    private String movieName;
     private String userId;
 }
