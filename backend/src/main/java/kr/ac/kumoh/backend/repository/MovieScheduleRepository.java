07e6d84e78d761ee0678edb26fa2811ea341aa4b
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-06-12 21:59:22 +0900

    add extra TestQuery

--- backend/src/main/java/kr/ac/kumoh/backend/repository/MovieScheduleRepository.java
+++ backend/src/main/java/kr/ac/kumoh/backend/repository/MovieScheduleRepository.java
@@ -34,4 +34,16 @@ public interface MovieScheduleRepository extends JpaRepository<MovieSchedule, Lo
     MovieSchedule getCertainMovieSchedule(@Param("theaterName") String theaterName,
                                           @Param("screenName") String screenName,
                                           @Param("screenTime") String screenTime);
+
+    // 특정 극장 특정 영화 상영 시간표 중 하나 + 영화 이름 확인
+    @EntityGraph(value = "MovieSchedule.Movie.Theater.graph", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("select ms from MovieSchedule ms " +
+            "where ms.screenTime = :screenTime and ms.movie.title = :movieName " +
+            "and ms.theater.screen = :screenName and ms.theater.theaterName = :theaterName")
+    MovieSchedule getMovieSchedule(
+            @Param("movieName") String movieName,
+            @Param("theaterName") String theaterName,
+            @Param("screenName") String screenName,
+            @Param("screenTime") String screenTime
+    );
 }
